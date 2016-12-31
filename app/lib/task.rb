d4de11ce4fcdf77bad65d0b1931a55807b228574
James Aspinwall <jaspinwa@akamai.com>
2016-12-31 14:26:13 -0500

    Task

--- app/lib/task.rb
+++ app/lib/task.rb
@@ -1,3 +1,34 @@
 class Task
-  
+  include Celluloid
+
+  def self.current
+    if @task.nil?
+      @task = new
+    else
+      @task
+    end
+  end
+
+  attr_accessor :timer
+
+  def schedule(n, block=nil, &b)
+    if block_given?
+      params = yield
+      @timer = after(n) {
+        puts "#{params}"
+        puts 'Here I am'
+      }
+    elsif block
+      @timer = after(n,&block)
+    else
+      @timer = after(n){
+        puts "Time: #{Time.now}"
+      }
+    end
+
+  end
+
+  def cancel
+    @timer.cancel
+  end
 end
