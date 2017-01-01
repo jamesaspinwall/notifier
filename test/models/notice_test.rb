9086b9c2806b5135636bbc136a2ca03b4fad7c77
James Aspinwall <jaspinwa@akamai.com>
2017-01-01 09:49:10 -0500

    validation notify_at

--- test/models/notice_test.rb
+++ test/models/notice_test.rb
@@ -3,24 +3,26 @@ require 'test_helper'
 class NoticeTest < ActiveSupport::TestCase
 
 
-    test 'notice1' do
-        Time.zone = 'America/Los_Angeles'
-        Chronic.time_class = Time.zone
-        #notify_at = Chronic.parse('tomorrow 10 am', context: :future)
-        notice = Notice.create notice_attr()
-        t = Time.zone.now.midnight + 11.hours + 1.day
-        assert_equal t, notice.notify_at
+  test 'notice1' do
+    Time.zone = 'America/Los_Angeles'
+    Chronic.time_class = Time.zone
+    assert_difference 'Notice.count' do
+      notice = Notice.create notice_attr()
     end
 
-    private
+    t = Time.zone.now.midnight + 11.hours + 1.day
+    assert_equal t, notice.notify_at
+  end
 
-    def notice_attr( attr = {})
-        {
-           title: 'title',
-           description: 'description',
-           notify_chronic: 'tomorrow 11 AM',
-        }.merge attr
+  private
 
-    end
+  def notice_attr(attr = {})
+    {
+      title: 'title',
+      description: 'description',
+      notify_chronic: 'tomorrow 11 AM',
+    }.merge attr
+
+  end
 
 end
